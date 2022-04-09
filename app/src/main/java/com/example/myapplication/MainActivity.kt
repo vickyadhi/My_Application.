98a1bb681ec1b97f502555153ba6b1ed76a50155
vignesh-pt5413 <vigneshmuniraj011@gmail.com>
2022-04-09 15:03:01 +0530

    Code pushed started

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.example.helloworld.adapter.ItemAdapter
+import com.example.helloworld.model.data.DataSource
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize data.
+        val myDataset = DataSource().loadAffirmations()
+
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        recyclerView.setHasFixedSize(true)
+    }
+}
